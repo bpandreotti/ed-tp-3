8f40a3911caffed6ba312998ecfc02fa5d8f15c4
Bruno Andreotti <bandreotti@protonmail.com>
2019-07-03 14:05:04 -0300

    Decodificar mensagem da entrada padrão

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "arvore.h"
 
 int main(int argc, char* argv[]) {
@@ -14,9 +15,19 @@ int main(int argc, char* argv[]) {
         arv.inserir_em_endereco(caractere, endereco);
     }
 
-    std::cout << arv.buscar_elemento("-.--")
-              << arv.buscar_elemento("..-.")
-              << arv.buscar_elemento("-.--") << std::endl;
+    std::string linha;
+    while (std::getline(std::cin, linha)) {
+        std::string s;
+        std::stringstream sstream(linha);
+        while (std::getline(sstream, s, ' ')) {
+            if (s == "/")
+                std::cout << " ";
+            else {
+                std::cout << arv.buscar_elemento(s);
+            }
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
