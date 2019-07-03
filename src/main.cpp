3659105c38a4716c34a3c9d13f8e4a55d05cf428
Bruno Andreotti <bandreotti@protonmail.com>
2019-07-03 13:16:44 -0300

    Ler morse.txt e contruir árvore

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-
+#include <fstream>
 #include "arvore.h"
 
 int main(int argc, char* argv[]) {
 
     Arvore arv = Arvore();
+    std::ifstream arquivo("morse.txt");
 
-    arv.inserir_em_endereco('A', "..-.");
-    arv.inserir_em_endereco('B', "-...");
-    arv.inserir_em_endereco('C', "-.-.");
+    char caractere;
+    std::string endereco;
 
-    std::cout << arv.buscar_elemento("..-.") << " "
-              << arv.buscar_elemento("-...") << " "
-              << arv.buscar_elemento("-.-.") << std::endl;
+    while (arquivo >> caractere >> endereco) {
+        arv.inserir_em_endereco(caractere, endereco);
+    }
+
+    std::cout << arv.buscar_elemento("-.--")
+              << arv.buscar_elemento("..-.")
+              << arv.buscar_elemento("-.--") << std::endl;
 
     return 0;
 }
